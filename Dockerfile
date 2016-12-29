e57d5f0cc1f99e021feed7bcb6dac95d2ce7e8ff
General-Meow <spl.ivalis@gmail.com>
2016-12-29 22:49:56 +0000

    update code

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM anapsix/alpine-java:8
 MAINTAINER Paul Hoang 2016-12-15
 RUN ["mkdir", "-p", "/home/javaapp"]
-COPY ./build/libs/mySharesCollector-1.0.jar /home/javaapp/mySharesCollector-1.0.jar
+COPY ./build/libs/mySharesCollector-1.1.jar /home/javaapp/mySharesCollector-1.1.jar
 WORKDIR /home/javaapp
 EXPOSE 5555
-ENTRYPOINT ["java", "-jar", "mySharesCollector-1.0.jar"]
+ENTRYPOINT ["java", "-jar", "mySharesCollector-1.1.jar"]
--- src/main/java/com.paulhoang/CollectController.java
+++ src/main/java/com.paulhoang/CollectController.java
@@ -41,39 +41,38 @@ public class CollectController {
         sharePrices.parallelStream().forEach(shareData -> {
             LOG.info("GOT SOME SHARE DATA: {}", shareData);
             final String recordString = GSON.toJson(shareData);
-//            final ProducerRecord record = new ProducerRecord<String, String>(appConfig.getTopic(), recordString);
-            final ProducerRecord record = new ProducerRecord<String, String>("topic", recordString);
+            final ProducerRecord record = new ProducerRecord<String, String>(appConfig.getTopic(), recordString);
             PRODUCER.send(record);
         });
         return "thanks!";
     }
 
     public static void main(String[] args) throws Exception {
-//        setupKafkaProducer();
+        setupKafkaProducer();
         SpringApplication.run(CollectController.class, args);
     }
 
-//    private static void setupKafkaProducer(){
-//        appConfig = loadConfiguration();
-//
-//        Properties properties = new Properties();
-//        properties.put("bootstrap.servers", appConfig.getMessageBusLocation());
-//        properties.put("acks", "all");
-//        properties.put("retries", 0);
-//        properties.put("batch.size", 16384);
-//        properties.put("linger.ms", 1);
-//        properties.put("buffer.memory", 33554432);
-//        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//        PRODUCER = new KafkaProducer(properties);
-//    }
+    private static void setupKafkaProducer(){
+        appConfig = loadConfiguration();
 
-//    private static AppConfig loadConfiguration() {
-//        final Yaml yaml = new Yaml();
-//        InputStream configInputStream;
-//            configInputStream = CollectController.class.getClassLoader()
-//                    .getResourceAsStream("appConfig.yml");
-//
-//        return yaml.loadAs(configInputStream, AppConfig.class);
-//    }
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", appConfig.getMessageBusLocation());
+        properties.put("acks", "all");
+        properties.put("retries", 0);
+        properties.put("batch.size", 16384);
+        properties.put("linger.ms", 1);
+        properties.put("buffer.memory", 33554432);
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        PRODUCER = new KafkaProducer(properties);
+    }
+
+    private static AppConfig loadConfiguration() {
+        final Yaml yaml = new Yaml();
+        InputStream configInputStream;
+            configInputStream = CollectController.class.getClassLoader()
+                    .getResourceAsStream("appConfig.yml");
+
+        return yaml.loadAs(configInputStream, AppConfig.class);
+    }
 }
